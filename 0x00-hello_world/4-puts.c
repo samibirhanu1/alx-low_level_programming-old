3b93c9a1ef388de2f5ac01d4e09e98bb7e44ae62
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-08 11:37:36 -0700

    pop

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
  */
 Int main(void)
 {
-	           puts("\ "Programming is like building a multilingual puzzle");
-                   return (0);
+      puts("\ "Programming is like building a multilingual puzzle");
+      return (0);
 }
 
