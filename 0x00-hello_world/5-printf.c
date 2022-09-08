4ef4b8888bdfcc20e4d250b1c680670ba6405352
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-08 11:49:25 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints a text
+ *
+ * Return: Always 0 (successful)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is piece of art, \n);
+	return (0);
+}
