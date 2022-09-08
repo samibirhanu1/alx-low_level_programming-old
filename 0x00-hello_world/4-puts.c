98271b4fc5d33df63d03a117a87db742d9a837a0
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-08 11:09:32 -0700

    trial for c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,10 @@
 *
 * Description: using the main function
 * this program prints "programming is like building a multilignual puzzle
-* return: 0
+* return: Always 0(successful)
 */
 int main(void)
 {
-         puts("\"Programming is like building a multilingual puzzle");
+         puts("\ "Programming is like building a multilingual puzzle");
          return(0);
 }
