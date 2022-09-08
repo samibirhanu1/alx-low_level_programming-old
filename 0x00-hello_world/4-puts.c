fea13ee112dc06c8520cddbd7d6735d46df25852
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-08 11:39:03 -0700

    what happened

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
  */
 Int main(void)
 {
-      puts("\ "Programming is like building a multilingual puzzle");
+      puts("\"Programming is like building a multilingual puzzle");
       return (0);
 }
 
