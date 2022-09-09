d358f6ba32ecac5b523ba8de6b8f588eb45b81f5
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-09 09:00:10 -0700

    with ct

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
-
-/**
- *Main – printing
- *
- *Return: Always 0 (successful)
- */
-Int main(void)
-{
-      puts("\"Programming is like building a multilingual puzzle");
-      return (0);
+#include <stdio.h>                                                   
+                                                                     
+/**                                                                  
+ *Main – printing                                                    
+ *                                                                   
+ *Return: Always 0 (successful)                                      
+ */                                                                  
+Int main(void)                                                       
+{                                                                    
+      puts("\"Programming is like building a multilingual puzzle");  
+      return (0);                                                    
 }
-
