bd6b6091a3c02e740d9906fd3d8bba4ef4320d9a
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-09 07:56:18 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (correct)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = ); < 10; i++) {
+		printf("%i", i);
+}
+
+        printf("\n");
+
+	return (0);
+}
