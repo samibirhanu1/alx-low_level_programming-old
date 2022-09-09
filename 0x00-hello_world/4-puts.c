b56df1abc874596c8657b58d46a3c5ee42794185
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-09 09:03:53 -0700

    why

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,13 @@
 /**                                                                  
  *Main – printing                                                    
  *                                                                   
- *Return: Always 0 (successful)                                      
+ *Return: Always 0 (successful/correct)                                      
  */                                                                  
+
 Int main(void)                                                       
+
 {                                                                    
       puts("\"Programming is like building a multilingual puzzle");  
+     
       return (0);                                                    
 }
