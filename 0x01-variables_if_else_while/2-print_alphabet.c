992997c72ef3d3977bc96429734540e74ae3bad2
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-10 11:34:35 -0700

    here we go

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,18 @@
-# include <stdlib.h>                                                                                                                  
-# include <time.h>                                                                                                                    
-# include <stdio.h>                                                                                                                   
-                                                                                                                                      
-/**                                                                                                                                   
- *  * main - Entry point                                                                                                                 
- *   *                                                                                                                                    
- *    * Return: Always 0 (success/correct)                                                                                                 
- *     */                                                                                                                                   
-int main(void)                                                                                                                        
-	                                                                                                                                      
-	        char alphabet;                                                                                                                
-		                                                                                                                                      
-		        for (alphabet = 'a'; alphabet <= 'z'; alphabet++) {                                                                           
-				                putchar(alphabet);                                                                                                    
-			}                                                                                                                                     
-                                                                                                                                      
-        putchar("\n");                                                                                                                
-	                                                                                                                                      
-	        return (0);                                                                                                                   
-		}
+# include <stdlib.h>                                                                                       # include <time.h>                                                                                         # include <stdio.h>
+     
+/**
+ * main -Entry pointReturn: 
+ *
+ * Always 0 (success/correct)
+ *
+ */                                                                                                                                   
+int main(void)
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+	putchar(alphabet);
+}
+putchar("\n");
+return (0);                                                                                                                   
+}
