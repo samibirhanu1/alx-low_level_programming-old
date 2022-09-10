2c67504ea9d203f8bf4662f93862090318ae0442
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-10 02:12:16 -0700

    not concentrating perfectly

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,5 +13,6 @@ int main(void)
 	printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
-	printf("Size of a float: %d byte(s)\n", (int) sizeof(float));	return (0);
+	printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
+	return (0);
 }
