4af068fe1d69bfb2d2e3f7ce996ccd38d2178d0d
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-10 11:39:22 -0700

    now i git this

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,7 @@
-# include <stdlib.h>                                                                                       # include <time.h>                                                                                         # include <stdio.h>
-     
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
 /**
  * main -Entry pointReturn: 
  *
