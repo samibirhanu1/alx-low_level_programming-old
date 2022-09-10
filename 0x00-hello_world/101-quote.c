6a569bccdd790c25e8b996b94154876e92ffcf08
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-10 02:50:19 -0700

    still needs focous understand concentrate off this all is getting better

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main - print a text last
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
