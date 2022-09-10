2ff487410cbaedaf0ee350c942ee6ecc27750093
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-10 10:48:42 -0700

    ssmth changed

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,8 +17,8 @@ int main(void)
         if (n == 0) 
 		printf("%i is zero\n", n);
 
-        else if (n < 0) 
-		printf("%i is negative\n", n);
+        else if (n > 0) 
+		printf("%i is postive\n", n);
 
         else 
 		printf("%i is postive\n,", n);
