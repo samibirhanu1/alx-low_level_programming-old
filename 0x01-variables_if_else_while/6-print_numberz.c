1c57b2a1695f2395935f294f0e847ab7cb62f809
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-10 12:22:47 -0700

    there is still many things i must know but at least now i stary understanding the basic and i'll continue and be the best

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -12,11 +12,12 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i <= 57; i++) {
-		putcher (i)
-}
+	for (i = 48; i <= 57; i++)
+       	{
+		putchar(i);
+	}
 
-                putchar ('\n')
+                putchar('\n');
 
 		return (0);
 
