a5614bacd17cd31ab7ccbba3f4a8b47c155464b6
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-10 11:10:19 -0700

    smth i can't get

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,13 +14,13 @@ int main(void)
        srand(time(0));
        n = rand() - RAND_MAX / 2;
 if (n == 0)  
-	printf("%i is zero\n", n);
+printf("%i is zero\n", n);
 
 else if (n > 0) 
-	printf("%i is positive\n", n);
+printf("%i is positive\n", n);
 
 else  
-	printf("%i is negative\n", n);
+printf("%i is negative\n", n);
 
         return (0);
 }
