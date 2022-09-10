37ae24e88edd655f80c511bd0ce4514e7d5f9749
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-10 12:04:43 -0700

    everything is essential in programming

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -17,7 +17,7 @@ int main(void)
 		putchar(lower_case);
 	}
 
-        for (upper_case = 'a'; upper_case <= 'z'; upper_case++) {
+        for (upper_case = 'A'; upper_case <= 'Z'; upper_case++) {
 		putchar(upper_case);
 	}
 
