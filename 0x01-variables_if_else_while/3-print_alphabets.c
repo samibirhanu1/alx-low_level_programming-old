891c5cd1c38b0ccd4ed839affdb543e365d352e9
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-10 12:01:50 -0700

    learning always

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,20 +8,21 @@
  * Return: Always 0 (success/correct)
  */
 int main(void)
+{
 
 	char lower_case;
 	char upper_case;
 
 	for (lower_case = 'a'; lower_case <= 'z'; lower_case++) {
 		putchar(lower_case);
-}
+	}
 
-        for (upper_case = "A"; upper_case <= "Z"; upper_case++) {
-		putcher(upper_case);
-}
+        for (upper_case = 'a'; upper_case <= 'z'; upper_case++) {
+		putchar(upper_case);
+	}
 
 
-        putchar("\n");
+        putchar('\n');
 	
 	return (0);
 }
