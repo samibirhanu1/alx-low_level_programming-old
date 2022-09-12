6207ddfef6bdbe0ff7c056ae3ae578644c849475
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-12 06:58:10 -0700

    betty is so compilicated cause as her name indicates she's a girl...

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,12 +18,10 @@ int main(void)
 		printf("%i is zero\n", n);
 	}
 	else if (n > 0)
-	{	
-		printf("%i is positive\n", n);
+	{printf("%i is positive\n", n);
 	}
 	else
-	{	
-		printf("%i is negative\n", n);
+	{       printf("%i is negative\n", n);
 	}
 	return (0);
 }
