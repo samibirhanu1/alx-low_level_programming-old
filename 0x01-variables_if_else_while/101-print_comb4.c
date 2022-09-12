54522147eecc371fc72e33dff8a464b4c2ce68b6
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-12 11:16:20 -0700

    love

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,10 +17,10 @@ int main(void)
 	{
 	putchar(p + '0');
 	putchar(m + '0');
-	}	
+}	
 	if (i != 7)
 		putchar(',');
-	        putchar(' ');
+	    	    putchar(' ');
 	putchar('\n');
 	return (0);
 }
