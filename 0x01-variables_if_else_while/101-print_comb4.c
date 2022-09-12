a90461034b67cebd73fbd4b1bb10ba8b300a84b1
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-12 11:06:51 -0700

    lopllkjuyt

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,15 +6,12 @@
  * main - Entry point
  *
  * Return: Always 0
- *
  */
 int main(void)
 {
-	int i, p, m;
-	
-	for (i = 1; i <=99; i++)
+	int i, p, m;																for (i = 1; i <= 99; i++)
 		for (p = 1; p <= 9; p++)
-			for (m = 2; m <=99; m++)
+			for (m = 2; m <= 99; m++)
 				if (m > p && p > i)
 					putchar(i + '0');
 	putchar(p + '0');
