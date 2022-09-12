eede22489b4f519f350bdeed3f08441753858612
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-12 02:09:23 -0700

    finished the next step is understanding all clearly and fix that betty style

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (correct)
+ */
+int main(void)
+{
+	int i, p;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (p = 0; p < 100; p++)
+		{
+			if (p > i)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((p / 10) + '0');
+				putchar((p % 10) + '0');
+				if (i != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
+
+
+
