9aa5a8bfa588627d690b3367134e89edae328a3d
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-12 08:29:02 -0700

    lk

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,7 +8,8 @@
  * Return: Always 0 (correct)
  */
 int main(void)
-{																		int i;																																		for (i = 0; i < 10; i++)
+{
+	int i;																																		for (i = 0; i < 10; i++)
 	{printf("%i", i);
 	}
 	printf("\n");																return (0);
