c3ae080cf949dcf162475ed8d57471f7e6b3c281
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-12 00:15:27 -0700

    learning

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,14 +12,14 @@ int main(void)
 	char base_16;
 
 	for (base_16 = 48; base_16 <= 57; base_16++) {
-		putchar(base_16)
+		putchar(base_16);
 }
 
         for (base_16 = 97; base_16 <= 102; base_16++) {
-		putchar(base_16)
+		putchar(base_16);
 }
 
-        putchar('\n')
+        putchar('\n');
 
 	return (0);
 }
