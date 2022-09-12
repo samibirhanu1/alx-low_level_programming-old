4eaa05ed33718c23f59a0617db75b35e2dfda677
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-12 09:08:43 -0700

    lion never eat grass

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -10,9 +10,8 @@
 int main(void)
 {
 	char alphabet;
-
+	
 	for (alphabet = 122; alphabet >= 97; alphabet--) 
-		putchar(alphabet);
-        putchar('\n');
+		putchar(alphabet);														putchar('\n');
 	return (0);
 }
