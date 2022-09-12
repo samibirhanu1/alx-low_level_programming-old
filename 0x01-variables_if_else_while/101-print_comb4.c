cc68acd647ef28b8001fd772bc1b74ad7ad244c8
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-12 01:56:48 -0700

    the aza part due to connection and pasting from word but still im grateful

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>                        
+#include <stdio.h>                         
+#include <time.h>                          
+/**                                        
+ *  * main - Entry point                      
+ *   *                                         
+ *    * Return: Always 0 (correct)              
+ *     *                                         
+ *      */                                        
+int main(void)                             
+{
+int i, p, m;       
+				                                           
+for (i = 1; i <=99; i++)
+for (p = 1; p <= 9; p++)
+{
+for (m = 2; m <=99; m++)
+if (m > p && p > i)
+{
+putchar(i + '0');
+putchar(p + '0');
+putchar(m + '0');
+if (i != 7)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
+
