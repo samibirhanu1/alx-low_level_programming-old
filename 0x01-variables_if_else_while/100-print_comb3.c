b55ade70a62553a115be58761549d8986e63d4a1
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-12 10:05:38 -0700

    loplo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,12 +8,12 @@
  */
 int main(void)
 {
-	int d;																	for (d = 0; d < 100; d++) 
-		putchar((d / 10) + '0');
-		putchar((d % 10) + '0');
-		if (d != 99)
-			putchar(',');
-			putchar(' ');
+	int d;
+	for (d = 0; d < 100; d++)putchar((d / 10) + '0');
+	putchar((d % 10) + '0');
+	if (d != 99)
+		putchar(',');
+	        putchar(' ');
 	putchar('\n');
 	return (0);
 }
