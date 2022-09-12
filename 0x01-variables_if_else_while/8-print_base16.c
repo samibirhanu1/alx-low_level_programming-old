cd9ca19e4dd801280dda3e641a78472f7664db9a
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-12 09:19:29 -0700

    lpoilo

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,13 +13,10 @@ int main(void)
 
 	for (base_16 = 48; base_16 <= 57; base_16++) {
 		putchar(base_16);
-}
-
-        for (base_16 = 97; base_16 <= 102; base_16++) {
+	}
+	for (base_16 = 97; base_16 <= 102; base_16++) {
 		putchar(base_16);
-}
-
-        putchar('\n');
-
+	}
+	putchar('\n');
 	return (0);
 }
