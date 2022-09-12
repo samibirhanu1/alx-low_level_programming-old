f75d2da9e1a6cece850e2585ea75ba31c919ea25
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-12 06:47:20 -0700

    trying to understand what betty needs

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,14 +13,19 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n == 0)  
+	if (n == 0)
+	{
 		printf("%i is zero\n", n);
+	}
 	
-	else if (n > 0) 
+	else if (n > 0)
+	{	
 		printf("%i is positive\n", n);
+	}
 	
-	else  
+	else
+	{	
 		printf("%i is negative\n", n);
-
-        return (0);
+	}
+	return (0);
 }
