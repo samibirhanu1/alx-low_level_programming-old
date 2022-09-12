d4d0bb2e3204ca42a243a958d786f2e494e1f045
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-12 11:12:22 -0700

    k

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,7 +15,7 @@ int main(void)
 				if (m > p && p > i)
 					putchar(i + '0');
 	putchar(p + '0');
-	putchar(m + '0');															if (i != 7)
+	putchar(m + '0');																if (i != 7)
 		putchar(',');
 	        putchar(' ');
 	putchar('\n');
