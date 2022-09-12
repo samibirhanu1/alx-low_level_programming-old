bd6bde969993a2ffe5c5a3cdcf06e6ad6cfc6b47
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-12 07:57:07 -0700

    ji

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,20 +9,18 @@
  */
 int main(void)
 {
-
 	char lower_case;
 	char upper_case;
 
-	for (lower_case = 'a'; lower_case <= 'z'; lower_case++) {
+	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+	{
 		putchar(lower_case);
 	}
 
-        for (upper_case = 'A'; upper_case <= 'Z'; upper_case++) {
+        for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
+	{
 		putchar(upper_case);
 	}
-
-
-        putchar('\n');
-	
+	putchar('\n');
 	return (0);
 }
