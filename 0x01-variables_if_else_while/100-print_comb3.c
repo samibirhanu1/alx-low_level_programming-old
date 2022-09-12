b7ded0b044b033f4e1aece61cb8fa363b8547ab8
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-12 09:49:14 -0700

    popop

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,14 +9,13 @@
 int main(void)
 {
 	int d;
+	
 	for (d = 0; d < 100; d++) 
 		putchar((d / 10) + '0');
 		putchar((d % 10) + '0');
 		if (d != 99)
-		{
 			putchar(',');
 			putchar(' ');
-		}
 	putchar('\n');
 	return (0);
 }
