adac5a6a2dbe644372c2037436f24151749f711d
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-12 10:27:52 -0700

    writting again

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,13 +8,26 @@
  */
 int main(void)
 {
-	int d;
-	
-	for (d = 0; d < 100; d++)putchar((d / 10) + '0');
-	putchar((d % 10) + '0');
-	if (d != 99)
-		putchar(',');
-	        putchar(' ');
-	putchar('\n');
+	int i, j;
+
+	for (i = 0; i<=9; i++)
+	{
+		for (j = 1; j <= 9; i++)
+		{
+			if (j < i)
+			{
+				putchar(i + '0')
+				putchar(i + '0')
+				if (i != 8)
+				{
+					putchar(',')
+					putchar(' ')
+				}
+			}
+		}
+	}
+	putchar('\n')
 	return (0);
 }
+
+
