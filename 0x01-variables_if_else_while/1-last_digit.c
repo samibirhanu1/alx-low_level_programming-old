42aa6a4d4fb1976d8f533eac8a6e30873c062297
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-12 07:07:12 -0700

    understanding betty while fixixng betty errors

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,14 +12,17 @@ int main(void)
 	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
-	if (n % 10 > 5) 
+	if (n % 10 > 5)
+	{
 		printf("Last digit of %i is %i and is greater than 5\n", n, n % 10);
-
-        else if (n % 10 == 0) 
-	        printf("Last digit of %i is %i and is 0\n", n, n % 10);
-
-        else 
+	}
+	else if (n % 10 == 0)
+	{
+		printf("Last digit of %i is %i and is 0\n", n, n % 10);
+	}
+	else
+	{	
 		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, n % 10);
+	}
 	return (0);
 }
