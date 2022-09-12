f187f1cf3e78c3c0af251c8a1ae551d0ed3cca6e
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-12 08:55:40 -0700

    understanding

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,20 +8,12 @@
  * Rweturn: Always 0 (succed/correct)
  */
 int main(void)
-
 {
 	int i;
 
 	for (i = 48; i <= 57; i++)
-       	{
-		putchar(i);
+	{	putchar(i);
 	}
-
-                putchar('\n');
-
-		return (0);
-
+       	putchar('\n');
+	return (0);
 }
-
-
-
