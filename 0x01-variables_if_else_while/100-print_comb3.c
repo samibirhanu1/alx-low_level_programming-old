a8e16f9f663344f5c47f81bf25cb7fcb1e1a2808
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-12 01:06:50 -0700

    coding but still not understanding clearly and fully after finishing the next step will be understanding

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 0; d < 100; d++) {
+		putchar((d / 10) + '0');
+		putchar((d % 10) + '0');
+	}
+
+	        if (d != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	putchar('\n');
+	return (0);
+}
