f60e735bc739d389e11f996a65bc271b50773b67
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-12 11:35:44 -0700

    ooplease oh pleas

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,17 +9,16 @@
  */
 int main(void)
 {
-	int i, p, m;																for (i = 1; i <= 99; i++)
-		for (p = 1; p <= 9; p++)
-			for (m = 2; m <= 99; m++)
-				if (m > p && p > i)
-				{
-					putchar(i + '0'); }
+	int i, p, m;
+	for (i = 1; i <= 99; i++)
+	for (p = 1; p <= 9; p++)
+	for (m = 2; m <= 99; m++)
+		if (m > p && p > i)putchar(i + '0');
 	putchar(p + '0');
 	putchar(m + '0');
 	if (i != 7)
 		putchar(',');
-		putchar(' ');
+	putchar(' ');
 	putchar('\n');
 	return (0);
 }
