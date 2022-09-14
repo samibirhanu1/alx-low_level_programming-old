4a5b78a3b6de54037ed4aff6659e37a3c6c95352
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-14 03:37:50 -0700

    vi s better

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,12 +3,12 @@
  *  * print_alphabet - function to print abc
  *   *
  *    * Return: 0
- *     */
+ */
 void print_alphabet(void)
 {
 		char c;
-
-			for (c = 'a'; c <= 'z'; c++)
+		
+		for (c = 'a'; c <= 'z'; c++)
 					{
 								_putchar(c);
 									}
