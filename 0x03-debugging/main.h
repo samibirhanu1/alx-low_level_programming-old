2c5c97b0926c41604d68a57edea728b1ea991b35
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-15 04:15:56 -0700

    task first

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,8 +4,5 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(int, int, int);
-void print_remaining_days(int, int, int);
-int convert_day(int, int);
 
 #endif /* MAIN_H */
