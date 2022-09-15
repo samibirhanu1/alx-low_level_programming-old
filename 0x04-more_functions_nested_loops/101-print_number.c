d769dfca7f0ca6dfb9a6deb7f3aebc4751564a9e
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-15 05:28:51 -0700

    last task task 12 advanced task 101 prime factor

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_number - Prints a number
+* @n: The number to print
+*/
+
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		putchar('-');
+		num = -num;
+	}
+	if (num > 9)
+	{
+		print_number(num / 10);
+	}
+	putchar(num % 10 + '0');
+}
