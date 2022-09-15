990814582c4124c69a3e1b3db79a633391eae72f
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-15 10:30:51 -0700

    frm otr

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,23 +1,21 @@
-#include <stdio.h>
-#include "main.h"
-
-/**
-* print_number - Prints a number
-* @n: The number to print
-*/
-
-void print_number(int n)
-{
-	unsigned int num = n;
-
-	if (n < 0)
-	{
-		putchar('-');
-		num = -num;
-	}
-	if (num > 9)
-	{
-		print_number(num / 10);
-	}
-	putchar(num % 10 + '0');
+#include "main.h" 
+/** 
+* print_number - prints numbers 
+* @n: number to be printed 
+* Return:void 
+*/ 
+ 
+void print_number(int n) 
+{ 
+unsigned int x; 
+ 
+x = n; 
+if (n < 0) 
+{ 
+_putchar(45); 
+x = -n; 
+} 
+if (x / 10) 
+print_number(x / 10); 
+_putchar((x % 10) + '0'); 
 }
