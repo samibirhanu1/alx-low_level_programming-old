02632c525adb2d8f5f32a0eb2cf930f834b71f77
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-16 04:42:36 -0700

    later on studying

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,16 +7,18 @@
  *
  * Return: Always 0 (success/correct)
  */
+
 int main(void)
 {
-	char alphabet;																																for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-	{
-	       if (alphabet == 'q')
-	               continue;
-	       else if (alphabet == 'e')
-	                continue;
-	       putchar(alphabet);
-	}
-	       putchar('\n');
-	       return (0);
+
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		if (alphabet == 'q')
+			continue;
+		else if (alphabet == 'e')
+			continue;
+	putchar(alphabet);
+	putchar('\n');
+	return (0);
 }
