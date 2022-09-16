408c8f880736c21d3abef4fa558dffcf4da53953
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-16 04:50:47 -0700

    again

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -14,12 +14,11 @@ int main(void)
 	char alphabet;
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-	
 		if (alphabet == 'q')
 			continue;
 		else if (alphabet == 'e')
 			continue;
-		putchar(alphabet);
-		putchar('\n');
-		return (0);
+	putchar(alphabet);
+	putchar('\n');
+	return (0);
 }
