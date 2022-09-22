30a2687fbd7d442951327dce3bbdd6039b08f4a0
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-22 02:20:32 -0700

    only betty problem 82 char

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,12 @@
+/*
+ * File: 0-strcat.c
+ * Auth: sam tech (samuel birhanu)
+ */
+
 #include "main.h"
 
 /**
- * strcat - Concatenates the string pointed to by @src, including the terminat
+ * strcat - Concatenates the string pointed to by @src, including the terminating
  *          null byte, to the end of the string pointed to by @dest.
  * @dest: A pointer to the string to be concatenated upon.
  * @src: The source string to be appended to @dest.
