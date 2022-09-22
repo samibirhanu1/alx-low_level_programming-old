fdffbd3ce0445ca76e434d37eda3e6ef6ac922d0
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-22 03:24:48 -0700

    doing experiment on task 10 magic

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -21,7 +21,5 @@ char *rot13(char *);
 void print_number(int n);                                                                                                               
 char *infinite_add(char *n1, char *n2, char *r, int size_r);                                                                            
 void print_buffer(char *b, int size);
-
-char *add_strings(char *n1, char *n2, char *r, int r_index);
                                                                                                                                         
 #endif
