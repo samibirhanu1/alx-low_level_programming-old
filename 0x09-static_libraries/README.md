97750b31cdc88f50a47d5a51f80507508ddb5cdd
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-09-30 04:00:57 -0700

    learning as f

--- 0x09-static_libraries/README.md
+++ 0x09-static_libraries/README.md
@@ -1,1 +1,1 @@
-two tasks of 0x09-static_libraries
+description here
--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: sam tech (samuel birhanu)
+ * desc: Header file containg the prototypes
+ *
+ */
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -20,4 +28,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
 #endif
