9d75f907d34e60af31303f6c6bdff91aeda5f7d6
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-10-19 22:00:05 -0700

    all tasks withour README.md

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void __attribute__((constructor)) hare(void);
+
+/**
+ * hare - Prints a string before the
+ *        main function is executed.
+ */
+void hare(void)
+{
+	printf("You're beat! and yet, you must allow,"
+	       "\nI bore my house upon my back!\n");
+}
+
