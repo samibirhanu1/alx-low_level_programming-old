3d28331003ab58bb05fb87e4db8a2f408bdc9ff6
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-10-27 00:21:28 -0700

    all tasks

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,32 +1,23 @@
 #include "main.h"
 
 /**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: A pointer to a string of 0 and 1 chars.
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
  *
- * Return: If b is NULL or contains chars not 0 or 1 - 0.
- *         Otherwise - the converted number.
+ * Return: the converted number
  */
-
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, mult = 1;
-	int len;
+	int i;
+	unsigned int dec_val = 0;
 
-	if (b == '\0')
+	if (!b)
 		return (0);
-
-	for (len = 0; b[len];)
-		len++;
-
-	for (len -= 1; len >= 0; len--)
+	for (i = 0; b[i]; i++)
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-
-		num += (b[len] - '0') * mult;
-		mult *= 2;
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-
-	return (num);
+	return (dec_val);
 }
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,6 @@
+here write a descripition that has sense and any description you like and to complete with these tasks follow the steps i'll tell you 
+use cat > file_name than write the codes there and to stop writting use ctrl + c ...
+i'll show you using vi but for u to write use cat
+after all understand the codes as that's the on;y intention everyonw want
+thanks
+make sure you see the whole video in order to havve no errors also like the video and subscribe to the channel to get new videos
